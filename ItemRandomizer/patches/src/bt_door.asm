f01edd398a2914f4a07596a4ac96b638dfb901e4
Thomas Backmark <total@viskos.org>
2017-06-29 08:46:31 +0200

    Renamed BT Door ips patch and modified the asm for xkas-plus

--- ItemRandomizer/patches/src/bt_door.asm
+++ ItemRandomizer/patches/src/bt_door.asm
@@ -1,13 +1,13 @@
-;door asm of bt/animal room during escape
+// door asm of bt/animal room during escape
+arch snes.cpu
 lorom
 
 org $8FEB8A
+    LDA $7ED820 //loads event flags
+    BIT #$4000  //checks for escape flag set
+    BEQ quit
+    LDA #$0015
+    STA $7E0946 //stores 15 to escape timer in seconds
 
-LDA $7ED820 ;loads event flags
-BIT #$4000  ;checks for escape flag set
-BEQ quit
-LDA #$0015
-STA $7E0946 ;stores 15 to escape timer in seconds
-
-quit:
-RTS
+    quit:
+    RTS 
